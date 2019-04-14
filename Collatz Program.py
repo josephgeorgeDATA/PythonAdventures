c8f1c09f5d2a9f005f0d149b575e0e7e3b54e716
justmailjoseph <49612664+justmailjoseph@users.noreply.github.com>
2019-04-14 22:48:49 +1000

    Add files via upload

---
+++ Collatz Program.py
@@ -0,0 +1,20 @@
+# Collatz Program
+
+def collatz(number):
+    if number % 2 == 0:
+        print (number // 2)
+        return number // 2
+    if number % 2 ==1:
+        print (3 * number + 1)
+        return 3 * number + 1
+
+print ('Enter a number ...')
+num = input()
+
+for x in range (1,1000):
+    num = collatz (int (num))
+    if num == 1:
+        break
+
+import sys
+print (sys.version)
